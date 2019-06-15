abe8c50aa81ce06a075720f6626732c745991463
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-15 01:09:25 -0400

    Added Hover Effect To Devices With Screen of at Least 850px and Removed From Others

--- javascript.js
+++ javascript.js
@@ -186,38 +186,3 @@ function generateQA() {
         }
     }
 }
-
-// Remove Hover From Mobile Devices
-function watchForHover() {
-    var hasHoverClass = false;
-    var container = document.body;
-    var lastTouchTime = 0;
-
-    function enableHover() {
-        // Filter Events From Touch
-        if (new Date() - lastTouchTime < 500) return;
-        if (hasHoverClass) return;
-
-        container.className += ' hasHover';
-        hasHoverClass = true;
-    }
-
-    function disableHover() {
-        if (!hasHoverClass) return;
-
-        container.className = container.className.replace(' hasHover', '');
-        hasHoverClass = false;
-    }
-
-    function updateLastTouchTime() {
-        lastTouchTime = new Date();
-    }
-
-    document.addEventListener('touchstart', updateLastTouchTime, true);
-    document.addEventListener('touchstart', disableHover, true);
-    document.addEventListener('mousemove', enableHover, true);
-
-    enableHover();
-}
-
-watchForHover();
